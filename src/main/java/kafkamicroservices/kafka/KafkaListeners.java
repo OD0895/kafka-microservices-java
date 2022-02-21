99b310a2eb0978013085bec0c17492be504e4a92
OSVALDO DANIEL AMADOR SALINAS <oamadors@ad.elektra.com.mx>
2022-02-21 15:04:37 -0600

    Creando una fabrica de Consumidores

---
+++ src/main/java/kafkamicroservices/kafka/KafkaListeners.java
@@ -0,0 +1,16 @@
+package kafkamicroservices.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(
+            topics = "exampleTopic",
+            groupId = "groupId"
+    )
+    void listener(String data) {
+        System.out.println("Listener received: " + data);
+    }
+}
