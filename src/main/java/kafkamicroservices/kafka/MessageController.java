f557259d3a8b47ec7eff66fbb35f935fad7d0ed2
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2022-02-21 21:09:34 -0600

    Creando contenedor de kafka

--- src/main/java/kafkamicroservices/kafka/MessageController.java
+++ src/main/java/kafkamicroservices/kafka/MessageController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/messages")
-public class MessageController {zc
+public class MessageController {
 
     private KafkaTemplate<String, String> kafkaTemplate;
 
